fb5e8b3148a35b5aa017c8b487c91151dcdfa600
jpcib <jpcibantos@gmail.com>
2023-03-19 22:56:29 -0300

    full test

---
+++ script/generate_rmd.R
@@ -0,0 +1,41 @@
+# test
+
+# create a vector of addresses
+addresses <- c("123 Main St", "456 Oak Ave", "789 Elm St", "321 Maple Ave")
+
+# create a vector of schools
+schools <- c("ABC Elementary", "XYZ Middle School", "123 High School", "456 Academy")
+
+# create a vector of ids
+ids <- 1:4
+
+# combine the vectors into a data frame
+dummy_data <- data.frame(Address = addresses, School = schools, ID = ids)
+
+# view the resulting data frame
+dummy_data
+
+
+# create a function to generate the .Rmd file for each school
+generate_rmd <- function(school_id) {
+  # get the school name and address from the data frame
+  school_name <- dummy_data$School[dummy_data$ID == school_id]
+  school_address <- dummy_data$Address[dummy_data$ID == school_id]
+  
+  # create the YAML header
+  yaml_header <- paste0("---\n", 
+                        "title: '", school_address, "'\n",
+                        "subtitle: '", school_name, "'\n",
+                        "---\n\n")
+  
+  # create the file name
+  file_name <- paste0(school_id, ".Rmd")
+  
+  # write the YAML header and some example content to the .Rmd file
+  writeLines(yaml_header, file_name)
+}
+
+# apply the function to each school in the data frame
+for (school_id in dummy_data$ID) {
+  generate_rmd(school_id)
+}
--- script/table_wrangle_test.R
+++ script/table_wrangle_test.R
@@ -2,22 +2,39 @@
 
 library(tidyverse)
 
-raw <- read_csv("data/base_resumida.csv") %>% 
+raw <- read_csv("data/base_completa.csv") %>% 
   glimpse()
 
 base_resumida_link <- raw %>% 
-  mutate("Comuna-Distrito" = paste0("C ",comuna, "- D.E. ",distrito)) %>% 
+  drop_na(cui) %>% #Hay que agregarle un cui a los NA (planetario, etc.). Ver como lo armo nico.
+  # mutate("Comuna-Distrito" = paste0("C ",comuna, "- D.E. ",distrito)) %>% 
   # rename(Domicilio = domicilio_edificio) %>% 
   mutate(Domicilio = paste0("<a href='https://jpcib.github.io/sistemaBochi/pages/",cui,".html","'>", domicilio_edificio,"</a>")) %>% 
-  mutate(CUI = paste0("<a href='https://jpcib.github.io/sistemaBochi/pages/",cui,".html","'>", cui,"</a>")) %>% 
-  select(-c(comuna,distrito,cui,domicilio_edificio)) %>% 
-  relocate(Domicilio,CUI,"Comuna-Distrito") %>% 
+  mutate(Establecimiento = paste0("<a href='https://jpcib.github.io/sistemaBochi/pages/",cui,".html","'>", nombre_establecimiento,"</a>")) %>%
+  rename(establecimiento = nombre_establecimiento) %>% 
+  rename(domicilio = domicilio_edificio) %>% 
+  # select(-any_of(c("domicilio_edificio"))) %>%
   glimpse()
 
 
-write_csv(base_resumida_link,"data/base_resumida_link.csv")
+write_csv(base_resumida_link,"data/base_completa_link_v2.csv")
 
 # mutate(fecha = as.Date(feed_pub_date),
 #        medio2 = as.factor(str_extract(item_link, "\\w*(?=\\.com|.net|.org|.gov|.gob)")), 
 #        titulo = paste0("<a href='",.$item_link,"'>",.$titulo,"</a>")) %>% 
 #   mutate(feed_title = as.factor(feed_title)) %>% 
+
+#Dummy data in qmd
+dummy_data <- read_csv("../data/base_resumida.csv") %>%
+  janitor::clean_names() %>%
+  rename(ID = cui,
+         Adress = domicilio_edificio
+  ) %>%
+  mutate(School = paste0(Adress," ",ID)) %>%
+  # slice_sample(n = 50) %>% 
+  # # group_by(ID) %>% 
+  # # mutate(n = n()) %>% 
+  # # filter(n >1) %>% 
+  distinct(ID, .keep_all = TRUE) %>%
+  drop_na(ID) %>% 
+  glimpse()
