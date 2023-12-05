0561cc69d012eae895ee21d33f88e358dfe9ea34
juan cibantos <jpcibantos@gmail.com>
2023-12-05 18:14:10 -0300

    2024 new infra mas mobi

---
+++ script/infra_wrangle_2024.R
@@ -0,0 +1,65 @@
+# infra_wrangle_v2
+library(tidyverse)
+
+gop23_raw <- read_csv("data/gop23_05-12.csv") %>%
+  # glimpse()
+  select(n_proyecto, proy, direccion, cui, intervenciones,
+         presupuesto_estimado = ppto_estimado, 
+         estado, subestado, fuente,
+         pase_dgar = contains("dgar"), inicio_obra, plazo_obra, ampliacion_plazo, 
+         avance_de_obra = avance_de_obra_real, fin_obra, 
+         # expediente, 
+         eje = programa, año_plan = ano_plan) %>% 
+  drop_na(proy) %>% 
+  glimpse()
+
+gop24_raw <- read_csv("data/gop24_05-12.csv") %>% 
+  select(n_proyecto, proy, direccion, cui, intervenciones,
+         presupuesto_estimado = ppto_estimado, 
+         estado, subestado, fuente,
+         pase_dgar = contains("dgar"), inicio_obra, plazo_obra, ampliacion_plazo, 
+         avance_de_obra = avance_de_obra_real, fin_obra, 
+         # expediente, 
+         eje = programa, año_plan = ano_plan) %>% 
+  mutate(across(everything(), ~as.character(.))) %>%
+  glimpse()
+
+gop23_left <- gop23_raw %>% 
+  anti_join(gop24_raw, by = c("proy","direccion")) %>% 
+  glimpse()
+
+gop_raw <- bind_rows(gop24_raw,gop23_left) %>% 
+  glimpse()
+
+# gop_raw  %>% 
+#   # select(-n_proyecto) %>% 
+#   janitor::get_dupes(proy,cui) %>% 
+#   # mutate(dup = duplicated()) %>% 
+#   view()
+
+plani_clean <- gop_raw %>% 
+  filter(!is.na(proy)) %>%
+  mutate(subestado = str_to_lower(subestado), 
+         cui = str_squish(cui), 
+         estado = str_to_title(estado), 
+         intervenciones = str_to_sentence(intervenciones)) %>% 
+  #saco obras con problemas en el cui
+  drop_na(cui) %>%
+  mutate(inicio_obra = case_when(is.na(inicio_obra) ~ "Sin fecha de inicio", 
+                                 T ~ inicio_obra), 
+         plazo_obra = case_when(is.na(plazo_obra) ~ "Sin plazo", 
+                                T ~ plazo_obra)) %>% 
+  
+  mutate(cui = str_pad(cui, 7, "left", "0")) %>% 
+  mutate(fin_obra = case_when(is.na(fin_obra) ~ " - ",
+                              fin_obra == "-" ~ " - ",
+                              T ~ fin_obra)
+         # fin_obra = case_when(n_proyecto == "x043" ~ "PARA RESCINDIR",
+         #                T ~ fin_obra)
+  ) %>% 
+  glimpse()
+
+write_csv(plani_clean, "data/infra_gop_05-12.csv")
+
+
+
